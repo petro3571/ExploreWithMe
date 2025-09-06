99601493eeec94dd612a42cd828605076145a7bd
PETRO <dim4petro357@yandex.ru>
2025-09-07 05:11:08 +0800

    checkstyle

--- main-service/src/main/java/ru/practicum/services/comment/CommentService.java
+++ main-service/src/main/java/ru/practicum/services/comment/CommentService.java
@@ -1,6 +1,5 @@
 package ru.practicum.services.comment;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import ru.practicum.dto.comments.CommentDto;
 import ru.practicum.dto.comments.NewCommentDto;
 
